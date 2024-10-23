e0a387c86576cb14b59e4b580f2d19e28cb1e276
Swas-tik <thawaliswastik@gmail.com>
2024-10-23 16:35:04 +0530

    variable assignment

---
+++ 01-variable-assignment.js
@@ -0,0 +1,16 @@
+
+var city;
+city = "Nagpur";
+console.log("My city name:",city)
+
+var country;
+country = "India";
+console.log("My country name:",country);
+
+var schoolName;
+schoolName = "P.T.S.M";
+console.log("My school name:",schoolName);
+
+var collageName;
+collageName = "Y.C.C.E";
+console.log("My collage name:",collageName);
---
+++ 01-variableAssign.js
@@ -0,0 +1,27 @@
+var myFullName;
+console.log("Full Name:",myFullName)
+
+myFullName= "Swastik Thawali";
+console.log("Full Name:",myFullName);
+
+myFullName= "Swastik Raju Thawali";
+console.log("Full Name:", myFullName);
+
+var myLove = "I love only JS";
+console.log("My love:",myLove);
+
+var firstName= "Swastik";
+console.log("First Name:",firstName)
+
+var lastName = "Thawali";
+console.log("Last Name:",lastName)
+
+var age= 26;
+console.log('Age:',age)
+
+var gender="male";
+console.log("gender:",gender)
+
+var isMarried = "false";
+console.log("Is married ?", isMarried)
+
