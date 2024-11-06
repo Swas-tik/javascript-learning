be42eacce7483fcf4b0b14eadb78aa1408d8d3d7
Swas-tik <thawaliswastik@gmail.com>
2024-11-06 19:25:38 +0530

    for assignment

---
+++ 06-for-assignment01.js
@@ -0,0 +1,32 @@
+console.log("Number from 5 to 15 incrementing 1")
+for(let i=5; i<=15; i++){
+  console.log(i)
+}
+
+console.log("Number from 50 to 40 decrementing 1")
+for(let i=50; i>=40; i--){
+  console.log(i)
+}
+
+console.log("first 15 odd number")
+for(let i=1; i<=30; i=i+2){
+  console.log(i)
+}
+
+console.log("first 10 event number")
+for(let i=2; i<=20; i=i+2){
+  console.log(i)
+}
+
+console.log("Table of 5")
+for(i=5; i<=50; i=i+5){
+  console.log(i)
+}
+console.log("Table of 10")
+for(i=10; i<=100; i=i+10){
+  console.log(i)
+}
+console.log("Table of 10 reverse order")
+for(i=100; i>=10; i=i-10){
+  console.log(i)
+}
